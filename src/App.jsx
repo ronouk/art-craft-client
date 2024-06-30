7d7c18c244f25011893981daa4d4dc03de8ebb2a
ronouk <shubro42@yahoo.com>
2024-06-30 18:09:35 +0600

    Initialized

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Heeellooo Jonogon</h1>
+      
+    </>
+  )
+}
+
+export default App
+
+/*
+
+proptype
+
+import PropTypes from 'prop-types';
+
+Do Proptypes validation this way:
+
+Component.propTypes = {
+  item1: PropTypes.type,
+  item2: PropTypes.type,
+  }
+
+  helmet
+
+  <HelmetProvider>
+	<Helmet>
+		<title>Title</title>
+	</Helmet>
+</HelmetProvider>
+
+*/
